fc0c685235c61e7d1c71293cccb20ada43e64e5b
Nemasha Anushani <nemasha.anushani2000@gmail.com>
2024-10-08 16:11:30 +0530

    comment here

--- diciSave.php
+++ diciSave.php
@@ -1,40 +1,30 @@
 <?php
- error_reporting(E_ALL ^ E_NOTICE);
- session_start();
- 
+error_reporting(E_ALL ^ E_NOTICE);
+session_start();
 
 if (!isset($_SESSION['admin_id'])) {
     header("Location: userlogin.php");
     exit();
-}else{
-    
-   
-?>
-
-<?php 
+}
 
 include 'db_connect.php';
 
-$empnumber = $_POST['empnumber'];
-$name = $_POST['name'];
-$reason = $_POST['reason'];
-
-$remark = $_POST['remark'];
+$empnumber = isset($_POST['empnumber']) ? $_POST['empnumber'] : '';
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$reason = isset($_POST['reason']) ? $_POST['reason'] : '';
+$remark = isset($_POST['remark']) ? $_POST['remark'] : '';
 $image_name = null;
 
 $response = array();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_FILES['photo']) && $_FILES['photo']['error'] == 0) {
-
         $targetDir = "dici/";
         $targetFile = $targetDir . basename($_FILES['photo']['name']);
         $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
         $allowedTypes = array('jpg', 'jpeg', 'png', 'gif');
 
-        // Check if file type is allowed
         if (in_array($imageFileType, $allowedTypes)) {
-            // Move the uploaded file to the target directory
             if (move_uploaded_file($_FILES['photo']['tmp_name'], $targetFile)) {
                 $image_name = basename($_FILES['photo']['name']);
                 $response['success'] = true;
@@ -56,11 +46,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $response['message'] = "Invalid request method.";
 }
 
-// Insert data into the 'discipline' table, including the image name if it was successfully uploaded
 if ($response['success']) {
-    $query = "INSERT INTO `discipline` (`emp_num`, `name`, `reason`, `img`, `remark`) VALUES ('$company','$empnumber', '$image_name', '$remark')";
+    $query = "INSERT INTO `discipline` (`emp_num`, `name`, `reason`, `img`, `remark`) VALUES ('$empnumber', '$name', '$reason', '$image_name', '$remark')";
 } else {
-    $query = "INSERT INTO `discipline` (`emp_num`, `name`, `reason`, `img`, `remark`) VALUES ('$company','$empnumber', NULL, '$remark')";
+    $query = "INSERT INTO `discipline` (`emp_num`, `name`, `reason`, `img`, `remark`) VALUES ('$empnumber', '$name', '$reason', NULL, '$remark')";
 }
 
 if (mysqli_query($con, $query)) {
@@ -68,11 +57,6 @@ if (mysqli_query($con, $query)) {
     exit();
 } else {
     echo "Error: " . mysqli_error($con);
-}
-
-?>
-
-<?php
 }
 
 ?>
